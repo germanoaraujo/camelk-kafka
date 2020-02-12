80ae59c12aa5c41a1d243eca1720b69d5baf3a86
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2020-02-12 15:49:47 -0300

    externalizing properties

--- CamelKafka.java
+++ CamelKafka.java
@@ -1,24 +1,16 @@
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.Exchange;
-import org.apache.camel.model.dataformat.JsonLibrary;
-import org.apache.camel.component.kafka.KafkaConstants;
-
 
 public class CamelKafka extends RouteBuilder {
 
   @Override
   public void configure() throws Exception {
 
-    String kafkaTopic = "my-topic";
-    String kafkaBootstrap = "my-cluster-kafka-bootstrap";
-    String kafkaDestination = "kafka:"+kafkaTopic+"?brokers="+kafkaBootstrap;
-
     from("timer:message-publisher")
       .setBody().simple("Publishing message to Kafka using CamelK")
-      .setHeader(KafkaConstants.KEY, constant("Camel")) // Key of the message
-    .to(kafkaDestination);
+      .setHeader("kafka.KEY", constant("Camel")) // Key of the message
+    .to("kafka:my-topic");
 
-    from(kafkaDestination)
+    from("kafka:my-topic")
       .log("Message received from Kafka : ${body}")
       .log("    on the topic ${headers[kafka.TOPIC]}")
       .log("    on the partition ${headers[kafka.PARTITION]}")
